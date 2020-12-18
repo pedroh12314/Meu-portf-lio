aacb0ab448deb65ff62ed903ffb907b2fdc0fa61
pedroh12314 <pedroh.12314@gmail.com>
2020-12-18 13:40:12 -0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+function openNav() {
+
+	var nav =  document.getElementById("navigation");
+	var profile =  document.getElementById("profile");
+
+	if (nav.className === "navigation") {
+		nav.className += " menujs";
+		document.getElementById("threeline-icon").innerHTML = "&Cross;";
+
+	} else {
+		nav.className = "navigation";
+		document.getElementById("threeline-icon").innerHTML = "&#9776;";
+
+	}
+
+
+
+}
